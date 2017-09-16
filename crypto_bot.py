390aec6729f61caa41369554d6b44be0da09a686
Jason Grout <grout58@gmail.com>
2017-09-16 00:12:16 +0000

    version .2 of script

--- crypto_bot.py
+++ crypto_bot.py
@@ -1,59 +1,30 @@
 import telebot, urllib.request, json
 
-bot = telebot.TeleBot("<TOKEN>")
+bot = telebot.TeleBot("190646199:AAF9VEdbhfy7a8zFCa7_TrPGP3gi_m0AQPk")
 
 user = bot.get_me()
 
 @bot.message_handler(commands=['help'])
 def print_help(message):
 	bot.reply_to(message, """
-	/eth - displays the price of Ether
-	/btc - displays the price of Bitcoin
-	/xmr - displays the price of Monero
-	/ltc - displays the price of Litecoin
-	/bcc - displays the price of Bitcoin Cash
+	To get the current price of any cryptocurrency type
+	in /<abbreviation>
+	IE /btc
+	IE /eth
 """)
 
-@bot.message_handler(commands =['eth'])
-def eth_price(message):
-	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR") as url:
-		data = json.loads(url.read().decode())
-		for currency, price in data.items():
+
+
+@bot.message_handler(func=lambda m: True)
+def all_price(message):
+	try:
+		crypto_abbrev = message.text.replace("/", "").upper()
+		with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym={}&tsyms=BTC,USD,EUR".format(crypto_abbrev)) as url:
+			data = json.loads(url.read().decode())
+			for currency, price in data.items():
 				if currency == 'USD':
-					bot.reply_to(message, "The price of Ethereum is currently {} {}".format(currency, price))
+					bot.reply_to(message, "The price of {} is currently {} {}".format(crypto_abbrev,currency, price))
 
-@bot.message_handler(commands =['btc'])
-def btc_price(message):
-	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,EUR") as url:
-		data = json.loads(url.read().decode())
-		for currency, price in data.items():
-				if currency == 'USD':
-					bot.reply_to(message, "The price of Bitcoin is currently {} {}".format(currency, price))
-
-@bot.message_handler(commands =['xmr'])
-def xmr_price(message):
-	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC,USD,EUR") as url:
-		data = json.loads(url.read().decode())
-		for currency, price in data.items():
-				if currency == 'USD':
-					bot.reply_to(message, "The price of Monero is currently {} {}".format(currency, price))
-
-@bot.message_handler(commands =['ltc'])
-def xmr_price(message):
-	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=BTC,USD,EUR") as url:
-		data = json.loads(url.read().decode())
-		for currency, price in data.items():
-				if currency == 'USD':
-					bot.reply_to(message, "The price of Litecoin is currently {} {}".format(currency, price))
-
-@bot.message_handler(commands =['bcc'])
-def bcc_price(message):
-	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym=BCC&tsyms=BTC,USD,") as url:
-		data = json.loads(url.read().decode())
-		for currency, price in data.items():
-				if currency == 'USD':
-					bot.reply_to(message, "The price of Bitcoin Cash is currently {} {}".format(currency, price))
-
-
-
+	except:
+		bot.reply_to(message, "Error, use / then abbreviation")
 bot.polling()
