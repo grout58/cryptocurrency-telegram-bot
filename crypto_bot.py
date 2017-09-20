5dd7e4ad95fb8201a85570c556f12214cd33ea9e
grout58 <grout58@gmail.com>
2017-09-19 21:38:05 -0400

    Add files via upload

--- crypto_bot.py
+++ crypto_bot.py
@@ -1,6 +1,9 @@
-import telebot, urllib.request, json
+import telebot
+import urllib.request
+import json
+import threading
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot("190646199:AAH27kykgbSYcmxfDkXfADDMkzi5FHXk0Lc")
 
 user = bot.get_me()
 
@@ -8,23 +11,36 @@ user = bot.get_me()
 def print_help(message):
 	bot.reply_to(message, """
 	To get the current price of any cryptocurrency type
-	in /<abbreviation>
+	in: /<abbreviation>
 	IE /btc
 	IE /eth
+	You can also receive prices in other currency by entering
+	the desired currency abbreviation after the currency
+	you're looking for:
+	IE "/btc eur" will give you the price of Bitcoin to Euros.
 """
-
+)
 
 
 @bot.message_handler(func=lambda m: True)
 def all_price(message):
 	try:
 		crypto_abbrev = message.text.replace("/", "").upper()
-		with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym={}&tsyms=BTC,USD,EUR".format(crypto_abbrev)) as url:
-			data = json.loads(url.read().decode())
-			for currency, price in data.items():
-				if currency == 'USD':
-					bot.reply_to(message, "The price of {} is currently {} {}".format(crypto_abbrev,currency, price))
+		crypto_abbrev_list = crypto_abbrev.split()
+		if len(crypto_abbrev_list) == 1:
+			crypto_abbrev_list.append('USD')
+			with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}".format(crypto_abbrev_list[0],crypto_abbrev_list[1])) as url:
+				data = json.loads(url.read().decode())
+				for currency, price in data.items():
+					bot.reply_to(message, "The price of {} is currently {} {}".format(crypto_abbrev_list[0],price, currency))
+		else:
+			with urllib.request.urlopen("https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}".format(crypto_abbrev_list[0],crypto_abbrev_list[1])) as url:
+				data = json.loads(url.read().decode())
+				for currency, price in data.items():
+					bot.reply_to(message, "The price of {} is currently {} {}".format(crypto_abbrev_list[0],price, currency))
+
+
 
 	except:
-		bot.reply_to(message, "Error, use / then abbreviation")
+	 	bot.reply_to(message, "Invalid format")
 bot.polling()
